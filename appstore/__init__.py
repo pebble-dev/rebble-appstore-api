b56c83005af2d688c87cc3a54f3610abfe7ee786
Katharine Berry <katharine@kathar.in>
2018-06-21 21:03:10 -0700

    Drop excessive query logging.

--- appstore/__init__.py
+++ appstore/__init__.py
@@ -7,8 +7,8 @@ from .api import init_app as init_api
 from .commands import init_app as init_commands
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 app = Flask(__name__)
 app.config.update(**config)
