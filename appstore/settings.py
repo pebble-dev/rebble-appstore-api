8f9f15b166bcaf47642c6a7fa4f55ada1d415534
Joshua Wise <joshua@accelerated.tech>
2019-06-17 02:45:56 -0400

    Use http to talk to auth on prod, to save a round trip.

--- appstore/settings.py
+++ appstore/settings.py
@@ -1,6 +1,7 @@
 import os
 
 domain_root = os.environ.get('DOMAIN_ROOT', 'rebble.io')
+http_protocol = os.environ.get('HTTP_PROTOCOL', 'https')
 
 config = {
     'DOMAIN_ROOT': domain_root,
@@ -8,7 +9,7 @@ config = {
     'PBW_ROOT': os.environ.get('PBW_ROOT', f'http://pbws.{domain_root}/pbw'),
     'IMAGE_ROOT': os.environ.get('IMAGE_ROOT', f'https://assets.rebble.io'),
     'APPSTORE_ROOT': os.environ.get('APPSTORE_ROOT', f'http://apps.{domain_root}'),
-    'REBBLE_AUTH_URL': os.environ.get('REBBLE_AUTH_URL', f'http://auth.{domain_root}'),
+    'REBBLE_AUTH_URL': os.environ.get('REBBLE_AUTH_URL', f"{http_protocol}://auth.{domain_root}"),
     'ALGOLIA_APP_ID': os.environ.get('ALGOLIA_APP_ID'),
     'ALGOLIA_ADMIN_API_KEY': os.environ.get('ALGOLIA_ADMIN_API_KEY'),
     'ALGOLIA_INDEX': os.environ.get('ALGOLIA_INDEX'),
