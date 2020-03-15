9e5d2bf0daeecc9c8a38e761219125f2117d67b8
Joshua Wise <joshua@accelerated.tech>
2020-03-14 20:35:03 -0400

    Sample requests.

--- appstore/__init__.py
+++ appstore/__init__.py
@@ -24,12 +24,34 @@ from .commands import init_app as init_commands
 from .utils import init_app as init_utils
 from .locker import locker
 
+from beeline.trace import _should_sample
+def sampler(fields):
+    sample_rate = 2
+
+    route = fields.get('route') or ''
+    if route == 'heartbeat':
+        sample_rate = 100
+    elif route == 'api.locker':
+        sample_rate = 10
+
+    method = fields.get('request.method')
+    if method != 'GET':
+        sample_rate = 1
+
+    response_code = fields.get('response.status_code')
+    if response_code != 200:
+        sample_rate = 1
+    
+    if _should_sample(fields.get('trace.trace_id'), sample_rate):
+        return True, sample_rate
+    return False, 0
+
 app = Flask(__name__)
 app.config.update(**config)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-if config['HONEYCOMB_KEY']:
-     beeline.init(writekey=config['HONEYCOMB_KEY'], dataset='rws', service_name='appstore-api')
-     HoneyMiddleware(app, db_events=True)
+if False:
+  beeline.init(writekey=config['HONEYCOMB_KEY'], dataset='rws', service_name='appstore-api', sampler_hook=sampler)
+  HoneyMiddleware(app, db_events=True)
 
 init_models(app)
 init_utils(app)
