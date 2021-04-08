47edb69f293606c59ba0312ada7169d114cf881d
Willow Systems <mail@willow.systems>
2021-04-08 16:34:08 +0100

    Return developer name instead of user name on /me/developer. Add POST request option to /me/developer to update developer name

--- appstore/dev_portal_api.py
+++ appstore/dev_portal_api.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import NoResultFound
 
 from .utils import authed_request, get_uid
-from .models import LockerEntry, UserLike, db, App
+from .models import LockerEntry, UserLike, db, App, Developer
 from .settings import config
 
 parent_app = None
@@ -42,15 +42,15 @@ def me():
     })
 
 
-@legacy_api.route('/users/me/developer')
+@legacy_api.route('/users/me/developer', methods=['GET'])
 def my_apps():
     result = authed_request('GET', f"{config['REBBLE_AUTH_URL']}/api/v1/me/pebble/appstore")
     if result.status_code != 200:
         abort(401)
     me = result.json()
     developer_id = me['id']
+    # Get apps
     my_apps = [x for x in App.query.filter_by(developer_id=developer_id)]
-    # Python is just weird javascript
     my_appdata = []
     for a in my_apps:
         my_appdata.append({
@@ -58,12 +58,52 @@ def my_apps():
             "title": a.title
         })
 
+    # Get developer name
+    developer = Developer.query.filter_by(id=developer_id).one()
+
     return jsonify({
-            'id': me['id'],
+            'id': me["id"],
+            'userid': me["uid"],
             'applications': my_appdata,
-            'name': me['name'],
+            'name': developer.name,
             'href': request.url,
     })
+
+@legacy_api.route('/users/me/developer', methods=['POST'])
+def update_my_developer():
+    # Validate request
+    permitted_fields = ["name"]
+
+    try:
+        req = request.json
+    except Exception as e:
+        print(e)
+        return jsonify(error = "Invalid POST body. Expected JSON", e = "body.invalid"), 400
+
+    if req is None:
+        return jsonify(error = "Invalid POST body. Expected JSON and 'Content-Type: application/json'", e = "request.invalid"), 400
+
+    for f in req:
+        if not f in permitted_fields:
+            return jsonify(error = f"Illegal field: {f}", e = "illegal.field"), 400
+
+    if not "name" in req:
+        return jsonify(error = f"Missing required field: name", e = "missing.field.name"), 400
+
+    # Resolve our auth token to our developer ID
+    result = authed_request('GET', f"{config['REBBLE_AUTH_URL']}/api/v1/me/pebble/appstore")
+    if result.status_code != 200:
+        abort(401)
+    me = result.json()
+    developer_id = me["id"]
+
+    developer = Developer.query.filter_by(id=developer_id).one()
+    print("Update developer from " + developer.name + " to " + req["name"])
+    developer.name = req["name"]
+    db.session.commit()
+
+    return jsonify(success = True, id = developer.id, name = developer.name)
+
 
 @legacy_api.route('/applications/<app_id>/add_heart', methods=['POST'])
 def add_heart(app_id):
