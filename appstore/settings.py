6cd2c7e16eb4c202efa8437f37e2cae613a9b64a
Joshua Wise <joshua@joshuawise.com>
2025-12-03 05:06:54 -0500

    modernize sqlalchemy to support new postgres

--- appstore/settings.py
+++ appstore/settings.py
@@ -5,7 +5,7 @@ http_protocol = os.environ.get('HTTP_PROTOCOL', 'https')
 
 config = {
     'DOMAIN_ROOT': domain_root,
-    'SQLALCHEMY_DATABASE_URI': os.environ['DATABASE_URL'],
+    'SQLALCHEMY_DATABASE_URI': os.environ['DATABASE_URL'].replace("postgres://","postgresql://"),
     'PBW_ROOT': os.environ.get('PBW_ROOT', f'http://pbws.{domain_root}/pbw'),
     'IMAGE_ROOT': os.environ.get('IMAGE_ROOT', f'https://assets.rebble.io'),
     'APPSTORE_ROOT': os.environ.get('APPSTORE_ROOT', f'http://apps.{domain_root}'),
